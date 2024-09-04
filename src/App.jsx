876c501634f4995f863a5a2b713c2d3e618eb51f
Lade Oshodi <13224175+ladeoshodi@users.noreply.github.com>
2024-09-04 10:11:52 +0100

    Create landing page component

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,55 @@
+import { useState } from "react";
 import "./App.css";
 
+const gameModes = [
+  {
+    label: "Classic",
+    value: "classic",
+  },
+  { label: "Hard", value: "hard" },
+];
+
 function App() {
+  const [gameMode, setGameMode] = useState(gameModes[0].value);
+
+  let title = "";
+  switch (gameMode) {
+    case "classic":
+      title = "Guess that Character!";
+      break;
+    case "hard":
+      title = "Guess that Actor!";
+      break;
+  }
+
+  function handleChange(e) {
+    setGameMode(e.target.value);
+  }
+
   return (
     <>
-      <div>This will be the Harry Potter Quiz Game</div>
-      <div>
-        View <a href="https://hp-api.herokuapp.com/">API Docs</a>
-      </div>
+      <nav>LeaderBoard</nav>
+      <section>
+        <h1>{title}</h1>
+      </section>
+      <button>Start Game</button>
+      <section>
+        <label htmlFor="game-mode">Game mode: </label>
+        <select
+          onChange={handleChange}
+          name="game-mode"
+          id="game-mode"
+          value={gameMode}
+        >
+          {gameModes.map((gameMode, index) => {
+            return (
+              <option key={index} value={gameMode.value}>
+                {gameMode.label}
+              </option>
+            );
+          })}
+        </select>
+      </section>
     </>
   );
 }
