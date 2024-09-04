431d1efb3b1de344b8ddb29c7cdccd30a0818e3b
Milo Chambers <chambersmilo@gmail.com>
2024-09-04 11:31:42 +0100

    Add browser router

---
+++ src/components/StartGame.jsx
@@ -0,0 +1,9 @@
+function StartGame() {
+  return (
+    <>
+      <h1>This is a Game Page</h1>
+    </>
+  );
+}
+
+export default StartGame;
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,17 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import App from "./App.jsx";
+import "./index.css";
+import StartGame from "./components/StartGame.jsx";
 
-createRoot(document.getElementById('root')).render(
+const router = createBrowserRouter([
+  { path: "/", element: <App /> },
+  { path: "/start-game", element: <StartGame /> },
+]);
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <RouterProvider router={router} />
+  </StrictMode>
+);
