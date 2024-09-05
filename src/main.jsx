621a50c3503ccc7b983475c1da15d3851190de9e
Lade Oshodi <13224175+ladeoshodi@users.noreply.github.com>
2024-09-05 18:29:04 +0100

    Update Leaderboard file path name to fix issue on Netlify

--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,18 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import App from "./App.jsx";
-import Leaderboard from "./components/LeaderBoard.jsx";
+import Leaderboard from "./components/Leaderboard.jsx";
 import "./index.css";
 
 const router = createBrowserRouter([
-  { path: "/", element: <App /> },
-  { path: "/leaderboard", element: <Leaderboard /> },
+  {
+    path: "/",
+    element: <App />,
+  },
+  {
+    path: "/leaderboard",
+    element: <Leaderboard />,
+  },
 ]);
 
 createRoot(document.getElementById("root")).render(
