13bc3470915fcfb63473ebdf6e54c621f5faddf3
Lade Oshodi <13224175+ladeoshodi@users.noreply.github.com>
2024-09-05 18:12:33 +0100

    Move import statements in main.jsx to trigger netlify build

--- src/main.jsx
+++ src/main.jsx
@@ -2,8 +2,8 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import App from "./App.jsx";
-import "./index.css";
 import Leaderboard from "./components/LeaderBoard.jsx";
+import "./index.css";
 
 const router = createBrowserRouter([
   { path: "/", element: <App /> },
